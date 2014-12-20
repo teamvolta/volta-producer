bc66a673e83f31568937b02ce5698ce9ee77e42e
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2014-12-19 19:40:38 -0800

    Made gulfile dryer

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var install = require('gulp-install');
 var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
-  // default task will be added here
-  gulp.src('./*.js')
-  .pipe(jshint('.jshintrc'))
-  .pipe(jshint.reporter('jshint-stylish'));
-  
-  gulp.src('./package.json') //gulp.src fetches the file and passes it on as an argument
-  .pipe(install());
+  gulp.start('style', 'install');  // default task can be added here
 });
+
+
+gulp.task('install', function() {
+   gulp.src('./package.json') //gulp.src fetches the file and passes it on as an argument
+  .pipe(install());
+})
 
 gulp.task('test', function() {  //I am still not sure what it actually does
 	                            // passing shared module in all tests (according to docs)
