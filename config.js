2d732b963d4a7c39db441fe101cc46298a9a3333
mal <lom.michael@gmail.com>
2014-12-19 17:54:35 -0800

    add current output

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 exports.development = {
   systemIp: 'localhost:8000',
   productionId: Math.random().toString(36).substr(2),
-  pricePerMWH: 5,
+  pricePerMWH: 2,
   minCapacity: 0,
-  maxCapacity: 300
+  maxCapacity: 100,
+  currentOutput 100
 }
 
 
