779ced482f2d3fd6c866d980823666864bf95028
mal <lom.michael@gmail.com>
2014-12-20 16:22:08 -0800

    refactored for pseudoclassical producer class

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 var config = require('./config')[process.argv[2]];
-var simulation = require('./simulation');
+var producer = new (require('./simulation'))(config);
 var socket = require('socket.io-client')(config.systemIp);
 
-//signal when connection is established and send productionId
+//signal when connection is established
 socket.on('connect', function(){
   console.log('producer online');
-  // This is called with no event, also, currently using socket.id for id anyways
-  // socket.emit({id: config.productionId});
-  // Report supply in intervals
 });
 
 //receives request from system admin for supply capacity(mwh) and price($/mwh) for bidding
 socket.on('requestSupply', function(){
   console.log('requestSupply')
-  var supply = simulation.getSupply();
+  var supply = producer.getSupply();
   // User socketId for now
   supply.producerId = socket.io.engine.id;
   socket.emit('reportSupply', supply);
@@ -21,8 +18,8 @@ socket.on('requestSupply', function(){
 
 //receives request from system admin to set capacity based on market-clearing price
 socket.on('changeProduction', function(data){
-  simulation.setCapacity(data)
+  producer.setCapacity(data)
   // Not needed right now
-  // socket.emit('reportSupply', simulation.setCapacity(data));
+  // socket.emit('reportCapacity', producer.setCapacity(data));
 });
 
