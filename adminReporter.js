47042840c6a97e4161c7d03912d1ac3a29b0395c
John Tan <tantai.john@gmail.com>
2014-12-22 14:56:32 -0800

    Integrated adminReported

---
+++ adminReporter.js
@@ -0,0 +1,32 @@
+var adminReporter = function(){
+  this.dataExport = [];
+  this.dataStore = {};
+};
+
+// Registers a value to watch. Pass in watcher callback that when called, returns the value to store
+adminReporter.prototype.register = function(key, watcher) {
+  if(!this.dataStore[key]){
+    this.dataStore[key] = {data:{}};
+    this.dataExport.push(this.dataStore[key].data);
+  };
+  this.dataStore[key].watcher = watcher;
+  this.dataStore[key].data.name = key;
+  this.dataStore[key].data.value = watcher();
+};
+
+// Updates all values and returns array of updated key value pairs.
+adminReporter.prototype.update = function() {
+  for(key in this.dataStore){
+    if(this.dataStore.hasOwnProperty(key)){
+      this.dataStore[key].data.value = this.dataStore[key].watcher();
+    }
+  };
+  return this.dataExport;
+};
+
+// Allows manual reporting of new levels
+adminReporter.prototype.report = function(key, getter) {
+  this.register(key, getter);
+};
+
+module.exports = adminReporter;
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 exports.development = {
+  port: 8001,
   systemIp: 'http://localhost:8000/producers',
   productionId: Math.random().toString(36).substr(2),
   pricePerMWH: 9001,
