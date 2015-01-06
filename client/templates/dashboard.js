682ffa192306d40276c5e373cf6a923b3716d22b
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-05 18:59:42 -0800

    Can store older transactions in the table

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -1,11 +1,8 @@
 angular.module('producerFrontEnd.dashboard', [])
 
 .controller('DashboardController', function ($scope, getSocket) {
-   angular.extend($scope, getSocket);
+  angular.extend($scope, getSocket);
    
-  $scope.ourOn('DashboardController', function (dataFromSocket) {
-      $scope.dataFromSocket = dataFromSocket;
-    });
   $('body').chartProduction($scope);
   $('body').chartPrice($scope);
   $('body').chartProfit($scope);
