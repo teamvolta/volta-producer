530b2c8d46918117a4dfa96f6501ea6a7d110898
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-06 16:36:11 -0800

    Added current capacity and costs values to the controls view

--- client/templates/controls.js
+++ client/templates/controls.js
@@ -1,6 +1,12 @@
 angular.module('producerFrontEnd.controls', [])
 
-.controller('ControlsController', function ($scope, sendControls) {
-  angular.extend($scope, sendControls); 
-  
+.controller('ControlsController', function ($scope, sendControls, getCapacityCosts) {
+  angular.extend($scope, sendControls);
+  angular.extend($scope, getCapacityCosts);
+  $scope.capCostsOn('ControlsController', function (data) {
+  	 $scope.$apply(function () {
+  	   $scope.capacity = data.capacity;
+       $scope.costs = data.costs;
+     });
+  });
 });
