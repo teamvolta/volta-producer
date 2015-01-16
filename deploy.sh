4a22f3d11767b90ba9cc8cdf7c35feba9ae8e4dd
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-16 12:01:23 -0800

    Modified the conditional file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 @echo off
 
-IF "%SITE_TYPE%" == "backend" (
-  deployback.sh
-) ELSE (
-  IF "%SITE_TYPE%" == "frontend" (
-    deployfront.sh
-  ) ELSE (
-    echo You have to set SITE_TYPE setting to either "backend" or "frontend"
-    exit /b 1
-  )
-)
+if [["%SITE_TYPE%" == "backend"]]; then
+  bash deployback.sh
+elif [["%SITE_TYPE%" == "frontend"]]; then
+  bash deployfront.sh
+else
+    echo "You have to set SITE_TYPE setting to either backend or frontend"
+    exit 1
+fi
