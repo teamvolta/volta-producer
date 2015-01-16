85d33179470a101345384f6680127e584370e9e9
mal <lom.michael@gmail.com>
2015-01-15 19:09:03 -0800

    reverted indenting for react

--- public/admin.js
+++ public/admin.js
@@ -28,11 +28,10 @@ var StatsBox = React.createClass({
     setInterval(this.loadStats.bind(this), 1000);
   },
   render: function() {
-    return ( < div className = "statsBox" >
-      < StatsList data = {
-        this.state.data
-      }
-      /> < /div>
+    return (
+      <div className = "statsBox">
+        <StatsList data = {this.state.data}/>
+      </div>
     )
   }
 });
@@ -40,33 +39,35 @@ var StatsBox = React.createClass({
 var StatsList = React.createClass({
   render: function() {
     var statNodes = this.props.data.map(function(stat) {
-      return ( < Stat name = {
-          stat.name
-        }
-        value = {
-          stat.value
-        } >
-        < /Stat>
+      return ( 
+        <Stat name = {stat.name} value = {stat.value} >
+        </Stat>
       )
     });
-    return ( < div className = "statsList" > {
-      statNodes
-    } < /div>)
+    return ( 
+      <div className = "statsList">
+        {statNodes}
+      </div>
+    )
   }
 });
 
 var Stat = React.createClass({
   render: function() {
-    return ( < div className = "stat" >
-      < h4 className = "statHeader" > {
-        this.props.name
-      } < /h4> < div > {
-        JSON.stringify(this.props.value)
-      } < /div> < /div>
+    return (
+      <div className = "stat">
+        <h4 className = "statHeader">
+          {this.props.name}
+        </h4>
+        <div >
+          {JSON.stringify(this.props.value)}
+        </div>
+      </div >
     )
   }
 });
 
-React.render( < StatsBox url = "api/stats" / > ,
+React.render(
+  <StatsBox url = "api/stats"/>,
   document.getElementById('content')
 )
