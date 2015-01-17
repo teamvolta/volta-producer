351ffe37dcf088369736d9c6e700037a84de5858
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2015-01-16 16:07:01 -0800

    Added more env variables to frontend server

--- client/frontServer.js
+++ client/frontServer.js
@@ -1,9 +1,10 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('../node_modules/express/lib/express.js');
 var config = require('../config')[process.env.NODE_ENV];
 var app = express();
 
 var server = require('http').Server(app);
-
 
 // Setup server.
 //
@@ -11,4 +12,4 @@ server.listen(config.portFront);
 
 app.use(express.static(__dirname));
 
-console.log("Running the front server file" + config.portFront);
+console.log("Running the front server file " + config.portFront);
